3f137ae37ca2d8eb82ada87a9b62c97bb0ed8a7c
BorisGPenev1 <121699311+BorisGPenev1@users.noreply.github.com>
2022-12-31 02:03:09 +0200

    Convorsation.py
    This is my first code EVER.

---
+++ Convorsation.py
@@ -0,0 +1,35 @@
+print("This project is open source")
+
+print("Hello this is a conversation.")
+
+print("This is Version 1.0")
+
+name = input ("Hello , what is your name?")
+print("Hello" + name + "!")
+
+
+age = input ("How old are you?")
+print("Ok your" + age + " years old!")
+
+
+sibling = input ("Do have any siblings?")
+print("Cool")
+
+
+sibling = input ("How many do you siblings have if you have none type 0")
+print("Cool" + sibling + " siblings !")
+
+country = input ("From what country are you from?")
+print("Cool you are from" + country + "!")
+
+support = input ("Thank you for downloading the project!")
+print("More projects on github")
+print("Please check out my other projects on github")
+
+
+add = input ("Suggest what to add this is just a project")
+
+
+
+
+
